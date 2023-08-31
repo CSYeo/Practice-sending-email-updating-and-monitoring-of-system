d8d4912912960eb6381530eb9e7c70b129aa3ef9
CSY <creightonstanleyyeo@gmail.com>
2023-08-31 13:04:19 +0800

    Corrected the size and no rotate and splitext typo

--- changeImage.py
+++ changeImage.py
@@ -4,16 +4,17 @@ from PIL import Image
 import os
 
 path = "{}/supplier-data/images".format(os.getcwd())
-size = (128,128)
+size = (600,400)
 
 def changeImage():
   new_photos = []
   for photo in os.listdir(path):
-    if photo != README:
+    if photo != "README" and photo != "LICENSE":
       im = Image.open("{}/{}".format(path,photo))
-      im = im.rotate(270).resize(size)
+      im = im.resize(size)
       im = im.convert('RGB')
-      new_path = os.path.splittext("{}/{}".format(path,photo))[0]
+      new_path = os.path.splitext("{}/{}".format(path,photo))[0]
+#splitext for split extension not splittext
       im.save("{}.jpeg".format(new_path), "JPEG")
       new_photos.append("{}.jpeg".format(new_path))
   return new_photos
