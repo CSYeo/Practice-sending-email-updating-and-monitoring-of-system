025435820b42c8e8c7c29c173f46404df41100bd
CSY <creightonstanleyyeo@gmail.com>
2023-08-31 12:49:16 +0800

    Bypass README file that was causing an issue

--- changeImage.py
+++ changeImage.py
@@ -3,18 +3,19 @@
 from PIL import Image
 import os
 
-path = os.getcwd()
+path = "{}/supplier-data/images".format(os.getcwd())
 size = (128,128)
 
 def changeImage():
   new_photos = []
-  for photo in os.listdir("~/supplier-data/images"):
-    im = Image.open("{}/images/{}".format(path,photo))
-    im = im.rotate(270).resize(size)
-    im = im.convert('RGB')
-    new_path = os.path.splittext("{}/images/{}".format(path,photo))[0]
-    im.save("{}.jpeg".format(new_path), "JPEG")
-    new_photos.append("{}.jpeg".format(new_path))
+  for photo in os.listdir(path):
+    if photo != README:
+      im = Image.open("{}/{}".format(path,photo))
+      im = im.rotate(270).resize(size)
+      im = im.convert('RGB')
+      new_path = os.path.splittext("{}/{}".format(path,photo))[0]
+      im.save("{}.jpeg".format(new_path), "JPEG")
+      new_photos.append("{}.jpeg".format(new_path))
   return new_photos
 
 def main():
